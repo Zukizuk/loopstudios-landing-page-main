0a99d23d923fc43ce183877763f3c729f9e547f0
Your Name <you@example.com>
2024-01-09 15:44:36 +0000

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,152 @@
+import "./App.scss";
+import Logo from "./images/logo.svg";
+import Menu from "./images/icon-hamburger.svg";
+import Close from "./images/icon-close.svg";
+import { MenuITems, socialMediaData, creationsData } from "./NavITems";
+import { useState } from "react";
+
+function App() {
+  const [active, setActive] = useState(false);
+
+  return (
+    <>
+      <header className="header">
+        <div className="header__container">
+          <a href="#" className="logo">
+            <img src={Logo} alt="Loopstudios - Logo" />
+          </a>
+          <nav className="nav">
+            <button
+              className={active ? "close__button" : "menu__button"}
+              type="button"
+              onClick={() => setActive(!active)}
+            >
+              <span className="sr-only">Menu</span>
+            </button>
+            <ul className={active ? "header__list active" : "header__list"}>
+              {MenuITems.map((item, index) => {
+                return (
+                  <li className="header__item" key={index}>
+                    <a href="#" className="header__link">
+                      {item.title}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main className="main">
+        <h1 className="sr-only">Loopstudios landing page</h1>
+        <section className="hero-banner">
+          <h2 className="hero-banner__heading">
+            Immersive experiences that deliver
+          </h2>
+        </section>
+        <section className="hero-section">
+          <div className="hero__illustrative"></div>
+          <div className="hero__block">
+            <h2 className="hero-section__heading">
+              The leader in interactive VR
+            </h2>
+            <p className="hero-section__description">
+              Founded in 2011, Loopstudios has been producing world-className
+              virtual reality projects for some of the best companies around the
+              globe. Our award-winning creations have transformed businesses
+              through digital experiences that bind to their brand.
+            </p>
+          </div>
+        </section>
+        <section className="creations">
+          <h2 className="creations__heading">Our creations</h2>
+
+          <a href="#" className="creations__action-btn">
+            See all
+          </a>
+
+          <ul className="creations__list">
+            {creationsData.map((item, id) => {
+              return (
+                <li className="creations__list-item" key={id}>
+                  <a href="#">
+                    <p>
+                      {item.firstLine} <span className="word-break"></span>
+                      {item.nextLine}
+                    </p>
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+      </main>
+
+      <footer>
+        <div className="footer__home-link">
+          <a href="#">
+            <img src={Logo} alt="Loopstudios" width="124" height="24" />
+          </a>
+        </div>
+        <nav className="footer__navigations">
+          <ul role="list" className="navigation__links">
+            {MenuITems.map((item, index) => {
+              return (
+                <li role="listitem" key={index}>
+                  <a href="#" className="footer__link">
+                    {item.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+        <nav className="footer__socials" aria-label="socials">
+          <ul role="list" className="socials__links">
+            {socialMediaData.map((item, id) => {
+              return (
+                <li role="listitem" key={id}>
+                  <a href="#" aria-label={item.label}>
+                    <img
+                      src={item.src}
+                      alt={item.alt}
+                      width={item.width}
+                      height={item.height}
+                    />
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+
+        <p className="footer__copyright">
+          © 2021 Loopstudios. All rights reserved.
+        </p>
+
+        <div className="attribution">
+          Challenge by
+          <a
+            href="https://www.frontendmentor.io/"
+            target="_blank"
+            aria-describedby="Frontend Mentor Website"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          , Coded by
+          <a
+            href="https://github.com/Zukizuk"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-describedby="Marzuk Entsie's Github profile"
+          >
+            Zuki
+          </a>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/NavITems.js
@@ -0,0 +1,68 @@
+import FB from "./images/icon-facebook.svg";
+import IG from "./images/icon-instagram.svg";
+import Pinterest from "./images/icon-pinterest.svg";
+import Twitter from "./images/icon-twitter.svg";
+
+export const MenuITems = [
+  {
+    title: "About",
+  },
+  {
+    title: "Careers",
+  },
+  {
+    title: "Events",
+  },
+  {
+    title: "Products",
+  },
+  {
+    title: " Support",
+  },
+];
+
+export const socialMediaData = [
+  {
+    id: 1,
+    src: FB,
+    alt: "Facebook - Img",
+    width: 20,
+    height: 20,
+    label: "Facebook",
+  },
+  {
+    id: 2,
+    src: Twitter,
+    alt: "Twitter - Img",
+    width: 20,
+    height: 17,
+    label: "Twitter",
+  },
+  {
+    id: 3,
+    src: Pinterest,
+    alt: "Pinterest - Img",
+    width: 20,
+    height: 20,
+    label: "Pinterest",
+  },
+  {
+    id: 4,
+    src: IG,
+    alt: "Instagram - Img",
+    width: 24,
+    height: 18,
+    label: "Instagram",
+  },
+];
+
+export const creationsData = [
+  { id: 1, firstLine: "Deep ", nextLine: "earth" },
+  { id: 2, firstLine: "Night ", nextLine: "arcade" },
+  { id: 3, firstLine: "Soccer team VR", nextLine: "" },
+  { id: 4, firstLine: "The ", nextLine: "grid" },
+  { id: 5, firstLine: "From up ", nextLine: "above VR" },
+  { id: 6, firstLine: "Pocket ", nextLine: "borealis" },
+  { id: 7, firstLine: "The ", nextLine: "curiosity" },
+  { id: 8, firstLine: "Make it ", nextLine: "fisheye" },
+];
